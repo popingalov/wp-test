dda278c310c947ba54490f391bc7b9a4c3a7c82e
Oleksandr Balitskiy <popingalov@gmail.com>
2021-09-19 14:54:56 +0300

    try

--- src/js/lk/index.js
+++ src/js/lk/index.js
@@ -1,1 +1,2 @@
 console.log('Personal cabinet');
+import { defaults } from '../../smart-gread-layer/smart-gread-layer';
--- src/smart-gread-layer/smart-gread-layer.js
+++ src/smart-gread-layer/smart-gread-layer.js
@@ -1,26 +1,27 @@
-import "./smart-gread-layer.scss";
+import './smart-gread-layer.scss';
 
-const columns = 12;   // number of grid columns
+const columns = 12; // number of grid columns
 export default function () {
-
-  let wiew_columns = "";
+  let wiew_columns = '';
   for (let i = 1; i <= columns; i++) {
-    wiew_columns += "<div></div>";
+    wiew_columns += '<div></div>';
   }
-  let grid = "<div class=\"degub-grid\"><div><div>" + wiew_columns + "</div></div></div><div id=\"gridToggle\">Toggle</div>";
+  let grid =
+    '<div class="degub-grid"><div><div>' +
+    wiew_columns +
+    '</div></div></div><div id="gridToggle">Toggle</div>';
 
-  document.querySelector("body").insertAdjacentHTML("beforebegin", grid);
+  document.querySelector('body').insertAdjacentHTML('beforebegin', grid);
 
-
-  const gridEnable = document.querySelector("#gridToggle");
-  const gridTable = document.querySelector(".degub-grid");
+  const gridEnable = document.querySelector('#gridToggle');
+  const gridTable = document.querySelector('.degub-grid');
   let trigger = true;
-  gridEnable.addEventListener("click", () => {
+  gridEnable.addEventListener('click', () => {
     if (trigger) {
-      gridTable.style.display = "block";
+      gridTable.style.display = 'block';
       trigger = false;
     } else {
-      gridTable.style.display = "none";
+      gridTable.style.display = 'none';
       trigger = true;
     }
   });
@@ -30,12 +31,12 @@ export default function () {
   };
 
   function resize_info() {
-    let sizeName = " XS ";
+    let sizeName = ' XS ';
     let sizeNow = document.documentElement.clientWidth;
-    if (sizeNow >= 576 && sizeNow < 768) sizeName = " SM ";
-    if (sizeNow >= 768 && sizeNow < 992) sizeName = " MD ";
-    if (sizeNow >= 992 && sizeNow < 1200) sizeName = " LG ";
-    if (sizeNow >= 1200) sizeName = " XL ";
+    if (sizeNow >= 576 && sizeNow < 768) sizeName = ' SM ';
+    if (sizeNow >= 768 && sizeNow < 992) sizeName = ' MD ';
+    if (sizeNow >= 992 && sizeNow < 1200) sizeName = ' LG ';
+    if (sizeNow >= 1200) sizeName = ' XL ';
     gridEnable.textContent = sizeName;
   }
 }
